c67d1ce9cc7cd5468b9993fb53444921feca8656
Fahad Esam <umnea@alMacBook-Pro-alkhas-bumnea.local>
2022-08-22 19:54:31 +0300

    firs commit

---
+++ mainFahad2.dart
@@ -0,0 +1,33 @@
+/*
+complete the below code using relational and arithmetic operator
+ */
+
+void main(List<String> args) {
+  int x = 5;
+  int y = 10;
+
+  print("is x equal y:");
+  print(x == y);
+
+  print("is x bigger than y:");
+  print(x > y);
+
+  print("is x not equal y:");
+  print(x != y);
+
+  print("is x less than or equal y:");
+  print(x <= y);
+
+  print("the value of 5 plus 2 is");
+  print(5 + 2);
+
+  print("the value of 3 multiply 6 is");
+  print(3 * 6);
+
+  print("the value of 5 divide by 2 is");
+  print(5 / 2);
+
+  print("the integer value of 5 divide by 2 is");
+
+  print(5 ~/ 2);
+}
